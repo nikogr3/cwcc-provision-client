b3cbb47d9a2f0aa598d61d18c657e19e587ffa15
Coty Condry <ccondry@cisco.com>
2019-07-23 13:31:59 -0500

    add more urls

--- src/urls.js
+++ src/urls.js
@@ -2,5 +2,14 @@
 
 module.exports = {
   team: '/api/auxiliary-data/resources/team',
-  user: '/api/security/users'
+  user: '/api/security/users',
+  routingStrategy: '/api/auxiliary-data/resources/routing-strategy',
+  virtualTeam: '/api/auxiliary-data/resources/virtual-team',
+  queue: '/api/auxiliary-data/resources/virtual-team',
+  site: '/api/auxiliary-data/resources/site',
+  userProfile: '/api/auxiliary-data/user-data/user-profile',
+  agentProfile: '/api/auxiliary-data/user-data/agent-profile',
+  skillProfile: '/api/auxiliary-data/user-data/skill-profile',
+  agentSkill: '/api/auxiliary-data/user-data/agent-skill',
+  tfn: '/api/auxiliary-data/resources/tfn'
 }
